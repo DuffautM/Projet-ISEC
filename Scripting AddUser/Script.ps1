ad7fff265d2e05b99cad4ee4ea06865045bd6fa9
Unknown <ludwig.maurinjollisgally@viacesi.fr>
2017-12-05 14:28:50 +0100

    Script to add users

---
+++ Scripting AddUser/Script.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module ActiveDirectory
+Import-Module 'Microsoft.PowerShell.Security'
+
+$users = Import-Csv -Delimiter "," -Path "./Groupe - Feuille 1.csv"
+
+foreach ($user in $users){
+    
+    
+    $Fname = $user.FirstName
+    $Initiales = $user.Initials
+    $Lname = $user.LastName
+    $Name = $user.FullName
+    $Login = $user.UserLogonName
+    $Groupe = $user.Groupe
+    $Upassword = $user.Password
+    
+    
+
+    switch($user.Groupe){
+        "gDirection" {$Groupe = "OU = Direction, DC = isec-group, DC = local, CN =$Name"}
+        "gMétier" {$Groupe = "OU = Métier, DC = isec-group, DC = local, CN =$Name"}
+        "gCommunication" {$Groupe = "OU = Communication, DC = isec-group, DC =local, CN =$Name"}
+        "gFinance" {$Groupe = "OU = Administration, DC = isec-group, DC =local, CN =$Name"}
+        "gRH"{$Groupe = "OU = RH, DC = isec-group, DC = local, CN =$Name"}
+        "tDirection" {$Groupe = "OU = tDirection, DC = tf42-group, DC = local, CN =$Name"}
+        "tTechnique" {$Groupe = "OU = tTechnique, DC = tf42-group, DC = local, CN =$Name"}
+        "tCommercial" {$Groupe = "OU = tCommercial, DC = tf42-group, DC = local, CN =$Name"}
+        "tAdminFinance" {$Groupe = "OU = tAdminFinance, DC = tf42-group, DC = local, CN =$Name"}
+        "tRH" {$Groupe = "OU = tRH, DC = tf42-group, DC = local, CN =$Name"}
+        default {$Groupe = $null}    
+    }
+    
+     try {
+            New-ADUser -Name $Name -SamAccountName $Login -UserPrincipalName $Login -DisplayName $Name -GivenName $Fname -Surname $Lname -AccountPassword (ConvertTo-SecureString $Upassword -AsPlainText -Force)  -Department $Groupe -Enabled $true
+            echo "Utilisateur ajouté : $Name"
+          
+           
+        } catch{
+            echo "utilisateur non ajouté : $Name"
+       }   
+
+   }
